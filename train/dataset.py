ddeadca83c153c408e42097d5e45381686ed0f90
Jacques <thibo.jacques@gmail.com>
2021-09-01 17:10:12 +0000

    Created a file for our dataset class.

---
+++ train/dataset.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.optim import Adam, lr_scheduler
+
+import rasterio
+
+
+class FloodDataset(Dataset):
+    """
+    Reads in images, transforms pixel values, and serves a
+    dictionary containing chip ids, image tensors, and
+    label masks (where available).
+    """
+
+    def __init__(self, x_paths, y_paths=None, transforms=None):
+        self.data = x_paths
+        self.label = y_paths
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # Loads a 2-channel image from a chip-level dataframe
+        img = self.data.loc[idx]
+        with rasterio.open(img.vv_path) as vv:
+            vv_path = vv.read(1)
+        with rasterio.open(img.vh_path) as vh:
+            vh_path = vh.read(1)
+        x_arr = np.stack([vv_path, vh_path], axis=-1)
+
+        # Min-max normalization
+        min_norm = -77
+        max_norm = 26
+        x_arr = np.clip(x_arr, min_norm, max_norm)
+        x_arr = (x_arr - min_norm) / (max_norm - min_norm)
+
+        # Apply data augmentation, if provided
+        if self.transforms:
+            x_arr = self.transforms(image=x_arr)["image"]
+        x_arr = np.transpose(x_arr, [2, 0, 1])
+
+        # Prepare sample dictionary
+        sample = {"chip_id": img.chip_id, "chip": x_arr}
+
+        # Load label if available - training only
+        if self.label is not None:
+            label_path = self.label.loc[idx].label_path
+            with rasterio.open(label_path) as lp:
+                y_arr = lp.read(1)
+            # Apply same data augmentation to label
+            if self.transforms:
+                y_arr = self.transforms(image=y_arr)["image"]
+            sample["label"] = y_arr
+            
+        return sample
