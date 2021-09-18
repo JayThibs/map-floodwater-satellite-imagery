9ceade24fa39067bbf690bc043b4fc050d5586d5
Jacques <thibo.jacques@gmail.com>
2021-09-18 01:39:50 +0000

    Trying to fix torch data type issue.

--- train/loss_functions.py
+++ train/loss_functions.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import tensor
 
 
 class XEDiceLoss(nn.Module):
@@ -14,8 +15,10 @@ class XEDiceLoss(nn.Module):
     def forward(self, pred, true):
         valid_pixel_mask = true.ne(255) # valid pixel mask
 
+        print(true)
+        print(type(true))
         # Cross-entropy loss
-        temp_true = torch.where((true == 255), 0, true) # cast 255 to 0 temporarily
+        temp_true = torch.where((true == 255).float(), tensor(0, dtype=torch.float64).cuda(), true) # cast 255 to 0 temporarily
         xe_loss = self.xe(pred, temp_true)
         xe_loss = xe_loss.masked_select(valid_pixel_mask).mean()
 
