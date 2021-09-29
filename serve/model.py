8c07b7354282586bcdfbe06d359bdd429d28b726
Jacques <thibo.jacques@gmail.com>
2021-09-29 22:23:31 +0000

    Removed outside code from predict and cleaned up imports.

--- serve/model.py
+++ serve/model.py
@@ -1,32 +1,7 @@
-from pathlib import Path
 import numpy as np
-import pandas as pd
-
 import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from torch.optim import Adam, lr_scheduler
-
 import pytorch_lightning as pl
-from pytorch_lightning.metrics.functional import iou
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, BackboneFinetuning
-from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
-
 import segmentation_models_pytorch as smp
-import albumentations as album
-import rasterio
-
-from metrics import intersection_and_union
-from loss_functions import XEDiceLoss
-
-from dataset import FloodDataset
-
-import numpy as np
-import pytorch_lightning as pl
-import rasterio
-import segmentation_models_pytorch as smp
-import torch
-
 
 class FloodModel(pl.LightningModule):
     def __init__(self):
@@ -45,27 +20,10 @@ class FloodModel(pl.LightningModule):
         # Forward pass
         return self.model(image)
 
-    def predict(self, vv_path, vh_path):
+    def predict(self, x_arr):
         # Switch on evaluation mode
         self.model.eval()
         torch.set_grad_enabled(False)
-
-        # Create a 2-channel image
-        with rasterio.open(vv_path) as vv:
-            vv_img = vv.read(1)
-        with rasterio.open(vh_path) as vh:
-            vh_img = vh.read(1)
-        x_arr = np.stack([vv_img, vh_img], axis=-1)
-
-        # Min-max normalization
-        min_norm = -77
-        max_norm = 26
-        x_arr = np.clip(x_arr, min_norm, max_norm)
-        x_arr = (x_arr - min_norm) / (max_norm - min_norm)
-
-        # Transpose
-        x_arr = np.transpose(x_arr, [2, 0, 1])
-        x_arr = np.expand_dims(x_arr, axis=0)
 
         # Perform inference
         preds = self.forward(torch.from_numpy(x_arr))
